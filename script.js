36f4b57033bba320fa494df907668cd6f8a00d09
Airton Zanon <me@airton.dev>
2024-04-22 11:58:36 +0200

    First Commit
    For now what this does is:
    
    Have an overview of what items you need (and have) in order to complete
    a quest in the quests page, so you don't need to click each quest to
    see this information.
    
    You see:
    Broccoli 176 100x
    Meaning:
    Item_Name Number_of_Items_on_inventory Number_of_items_required_for_quest
    
    Signed-off-by: Airton Zanon <me@airton.dev>

---
+++ script.js
@@ -0,0 +1,73 @@
+function trigger() {
+    const questPages = document.querySelectorAll('[data-page="quests"]');
+    
+    const requiredItems = [];
+    
+    if (questPages.length > 0) {
+      const links = questPages[0].querySelectorAll('a');
+      const linkList = Array.from(links)
+        .filter((link) => link.href.includes('quest.php?id='));
+    
+      linkList.forEach((linkContent) => {
+        setTimeout(() => {
+        const link = linkContent.href;
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', link + '&referer=farmrpghelper', true);
+    
+    
+        xhr.onload = function () {
+          if (this.status >= 200 && this.status < 400) {
+            const questPageHtml = this.responseText;
+            const parser = new DOMParser();
+            const questPageDoc = parser.parseFromString(questPageHtml, 'text/html');
+    
+            const requiredItemsContainer = questPageDoc.querySelectorAll('a[href^="item.php?id="][href*="&needed="]');
+            requiredItemsContainer.forEach((item) => {
+              const itemName = item.textContent.trim();
+              const itemString = itemName.replace(/\s+/g, " ").replace(' You have', ':');
+    
+              const itemTitleElement = linkContent.querySelector('.item-title');
+              if (itemTitleElement) {
+                itemTitleElement.innerHTML += '<br><span style="font-size: 11px; color:yellow;"><strong>' + itemString + '</strong></span>';
+              } else {
+                console.warn("Element with class '.item-title' not found in the DOM.");
+              }
+            });
+          }
+        };
+    
+        xhr.onerror = function () {
+          console.error(`Failed to fetch quest page: ${link}`);
+        };
+    
+        xhr.send();
+    
+        }, 1000);
+      });
+    } else {
+      console.log('No elements found with data-page="quests".');
+    }
+}
+
+const callback = (mutationList, observer) => {
+  let pass = 0;
+  for(let mutation of mutationList) {
+    if (mutation.type === 'attributes' && mutation.attributeName === 'data-page' && mutation.target.dataset.page === 'quests') {
+      if (pass >= 1) {
+        continue;
+      }
+      trigger();
+      pass += 1;
+    }
+  }
+};
+
+const observer = new MutationObserver(callback);
+
+const targetElement = document.querySelector('#fireworks');
+
+if (targetElement) {
+  observer.observe(targetElement, { attributes: true, attributeFilter: ['data-page'] });
+} else {
+  console.log('No element found with the specified id.');
+}
